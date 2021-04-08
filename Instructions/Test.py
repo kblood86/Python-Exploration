dd73fbc6747effaf8b385a536351748744f1c5d9
Katie Berkowitz <katieb@Katies-MacBook-Pro.local>
2021-04-08 10:28:52 -0400

    adding instructions and extra content

---
+++ Instructions/Test.py
@@ -0,0 +1,20 @@
+import os
+import csv
+
+csvpath = os.path.join('..', 'PyBank / Resources', 'budget_data.csv')
+
+
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    # Read the header row first 
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    for row in csvreader:
+        print(row)
